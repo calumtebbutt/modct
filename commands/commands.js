be17d54fca0d332aef2111fc95c5e9f1cd0cb22f
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-06-25 22:44:54 +0100

    created command.js

--- commands/commands.js
+++ commands/commands.js
@@ -1,1 +1,10 @@
+const { Client, Collection, MessageEmbed } = require('discord.js');
 
+module.exports.config = {
+    name: "commands" or "help"
+    aliases: []
+}
+
+module.exports.run = async (client, message, args) => {
+  message.author.send("\n\n**Commands**\n\n**!commmands** or **help** - to view commands\n**!serverinfo** - to view server information\n\n**!kick @user** - to kick a specified member\n\n**!ban @user** - to ban a specified member")
+}
