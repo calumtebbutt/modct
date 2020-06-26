009ef1571a8549d2ed3cf9ca8812699802c6dd3a
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-06-26 17:50:55 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+const { Client, Collection, MessageEmbed } = require('discord.js');
+
+module.exports.config = {
+    name: "help",
+    aliases: []
+}
+
+module.exports.run = async (client, message, args) => {
+  message.reply(" check DMs!")
+  message.author.send("\n\n**Commands**\n\n**!commands** or **!help** - to view commands\n**!serverinfo** - to view server information\n**!kick @user** - to kick a specified member\n**!ban @user** - to ban a specified member")
+}
