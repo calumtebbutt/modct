ec8550fcd7a67f09258d87e8f737b382ab222c02
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-06-26 17:51:31 +0100

    !help addition

--- commands/commands.js
+++ commands/commands.js
@@ -7,5 +7,5 @@ module.exports.config = {
 
 module.exports.run = async (client, message, args) => {
   message.reply(" check DMs!")
-  message.author.send("\n\n**Commands**\n\n**!commands** - to view commands\n**!serverinfo** - to view server information\n**!kick @user** - to kick a specified member\n**!ban @user** - to ban a specified member")
+  message.author.send("\n\n**Commands**\n\n**!commands** or **!help** - to view commands\n**!serverinfo** - to view server information\n**!kick @user** - to kick a specified member\n**!ban @user** - to ban a specified member")
 }
