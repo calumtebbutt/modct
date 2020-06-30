cb89c8810083565573ea1e0a9a32b8a2f6a856b5
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-06-30 20:25:37 +0100

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -1,4 +1,4 @@
-const { Client, Collection, MessageEmbed, CategoryChannel, DiscordAPIError, GuildEmoji } = require('discord.js');
+const { Client, Collection, MessageEmbed } = require('discord.js');
 
 module.exports.config = {
     name: "serverinfo",
@@ -6,16 +6,20 @@ module.exports.config = {
 }
 
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
+
     let serverembed = new MessageEmbed()
     .setDescription("Server Information")
     .setColor("#15f153")
     .setThumbnail(message.guild.iconURL)
     .addField("Server Name", message.guild.name)
+    .addField("Region", `${message.guild.region}`)
     .addField("Created On", message.guild.createdAt)
     .addField("You Joined", message.member.joinedAt)
     .addField("Total Members", message.guild.memberCount)
-    .addField("Owner", message.guild.owner);
+    .addField("Owner", message.guild.owner)
+    .addField("Channels", `${message.guild.channels.cache.size}`)
+    .addField("Roles", `${message.guild.roles.cache.size}`);
 
     message.channel.send(serverembed);
 }
