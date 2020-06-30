e1a8a02940804f30725d1e6d1f784991a774b3c8
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-06-30 20:27:46 +0100

    Create servercount.js

---
+++ commands/creator/servercount.js
@@ -0,0 +1,21 @@
+const { Client, Collection, MessageEmbed } = require('discord.js');
+
+module.exports.config = {
+    name: "smc",
+    aliases: []
+}
+
+
+module.exports.run = async (client, message, args) => {
+    ownerID = ('267388385159675904')
+
+    if(message.author.id == ownerID) {
+
+        let serversize = client.guilds.cache.size
+        let membersize = client.users.cache.size
+
+        message.channel.send(`**CTMod** is currently in **${serversize} servers watching ${membersize} users.**`)
+    } else {
+        message.reply(" you do not have permission to use this command.")
+    }
+}
