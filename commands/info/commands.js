70e1b052d7e94227cf5ddc7404433cd9cfe3842b
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-07-01 18:19:26 +0100

    embed update

--- commands/info/commands.js
+++ commands/info/commands.js
@@ -6,6 +6,32 @@ module.exports.config = {
 }
 
 module.exports.run = async (client, message, args) => {
+    const commandEmbed = new MessageEmbed()
+    .setTitle("CTMod Commands")
+    .setDescription("Find a full list of commands here! *Please note some commands require certain permissions to work*")
+    .setColor("#09FF00")
+    .addField("!announce #channel <message>", "Announces a message in a channel")
+    .addField("!ban @user <reason>", "Bans the mentioned user")
+    .addField("!kick @user <reason>", "Kicks the mentioned user")
+    .addField("!mute @user <reason>", "Mutes the mentioned user")
+    .addField("!unmute @user", "Unmutes the mentioned user")
+    .addField("!nickname @user <name>", "Changes mentioned user's nickname")
+    .addField("!purge <number>", "Bulk deletes specified number of messages")
+    .addField("!slowmode <number>", "Sets slowmode of the channel to desired amount (seconds)")
+    .addField("!botinfo", "Displays information about CTMod")
+    .addField("!commands or !help", "Sends a list of commands")
+    .addField("!donate", "Sends a link to donate to CTModeration")
+    .addField("!invite", "Sends an invite link for CTModeration")
+    .addField("!membercount", "Displays how many members are in the server")
+    .addField("!ping", "Shows Latency & API Latency")
+    .addField("!serverinfo", "Displays information about the server")
+    .addField("!userinfo @user", "Displays information about mentioned user")
+    .addField("!suggest <suggestion>", "Creates a suggestion")
+    .setFooter("CTModeration");
+    
+    
+    
+   
   message.reply(" check DMs!")
-  message.author.send("\n\n**Commands**\n\n**!commands** or **!help** - to view commands\n**!serverinfo** - to view server information\n**!kick @user** - to kick a specified member\n**!ban @user** - to ban a specified member\n**!donate** - to support CTModeration\n**!premium** - to buy CTMod Premium\n**!botinfo** - to view information on CTMod\n**!ping** - to view Latency & API Latency\n**!uptime** - how long since the last CTMod update.\n**!invite** - invite CTMod to your server\n**!nickname @user <name>** - to change a users nickname\n**!announce #channel <message>** - to send an message from the bot in a specified channel\n**!mute @user <reason>** - to mute a user\n**!unmute @user** - to unmute a user\n**!purge <number>** - to bulk delete an amount of messages\n**!slowmode <seconds>** - to turn slowmode on in a channel to a custom time\n**!userinfo @user** - to view information on a user\n**!eval** - Creator Locked command")
+  message.author.send(commandEmbed)
 }
