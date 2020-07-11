422eaea8e5b3e206c935e65fe577d85804c4ce55
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-07-11 22:32:01 +0100

    Update pilotapp.js

--- commands/groups/redbull/pilotapp.js
+++ commands/groups/redbull/pilotapp.js
@@ -7,15 +7,16 @@ module.exports.config = {
 
 module.exports.run = async (client, message, args) => {
 
-    if(message.guild.id === "720999183905063012"){
-        let supmember = message.mentions.members.first()
+    if (message.guild.id === "720999183905063012") {
+        if (message.member.hasPermission("ADMINISTRATOR")) {
+            let supmember = message.mentions.members.first()
 
 
 
-    let announcementchannel = message.guild.channels.cache.find(c => c.name === "team-chat")
-    if (!announcementchannel) return;
+            let announcementchannel = message.guild.channels.cache.find(c => c.name === "team-chat")
+            if (!announcementchannel) return;
 
-    announcementchannel.send(`${supmember}, Hello. I am glad to say you have passed your application and have been entered into the training program. If you didn't already know the training program lasts 4 weeks where you will be constantly evaluated. From the performance and ability we see in the trainings you attend, it will determine your full place on the team.`);
-
+            announcementchannel.send(`${supmember}, Hello. I am glad to say you have passed your application and have been entered into the training program. If you didn't already know the training program lasts 4 weeks where you will be constantly evaluated. From the performance and ability we see in the trainings you attend, it will determine your full place on the team.`);
+        } else message.reply(", you do not have permission to use this command.")
     } else return;
 }
