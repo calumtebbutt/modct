fa966f54bec3145972b7eb1a1cd4f65c06aeb969
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-07-12 00:13:26 +0100

    Update engineerapp.js

--- commands/groups/redbull/engineerapp.js
+++ commands/groups/redbull/engineerapp.js
@@ -9,9 +9,11 @@ module.exports.run = async (client, message, args) => {
 
     if(message.guild.id === "720999183905063012"){
         if(message.member.hasPermission("ADMINISTRATOR")){
-            let supmember = message.mentions.members.first()
-
-
+            let supmember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        
+            if(!supmember){
+                message.reply(", please mention a user.")
+            }
 
             let announcementchannel = message.guild.channels.cache.find(c => c.name === "team-chat")
             if (!announcementchannel) return;
