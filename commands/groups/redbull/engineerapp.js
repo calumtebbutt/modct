f526b131298d92a5910630080299b2a916fe9433
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-07-11 22:44:49 +0100

    Update engineerapp.js

--- commands/groups/redbull/engineerapp.js
+++ commands/groups/redbull/engineerapp.js
@@ -15,6 +15,7 @@ module.exports.run = async (client, message, args) => {
 
             let announcementchannel = message.guild.channels.cache.find(c => c.name === "team-chat")
             if (!announcementchannel) return;
+            message.delete();
         
             announcementchannel.send(`${supmember}, Hello. I am glad to announce that you have passed your application to become an engineer and have been entered into the training program. If you didn't already know the training program lasts 4 weeks where you'll be constantly evaluated. From the performance and ability we see in the trainings you attend, it will determine your full place on the team.`);
         
