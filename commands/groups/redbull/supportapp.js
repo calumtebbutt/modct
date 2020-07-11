c3cf022b2c9520f968e570b7bad412c637ff38a4
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-07-11 22:32:14 +0100

    Update supportapp.js

--- commands/groups/redbull/supportapp.js
+++ commands/groups/redbull/supportapp.js
@@ -7,15 +7,16 @@ module.exports.config = {
 
 module.exports.run = async (client, message, args) => {
 
-    if(message.guild.id === "720999183905063012"){
-        let supmember = message.mentions.members.first()
+    if (message.guild.id === "720999183905063012") {
+        if (message.member.hasPermission("ADMINISTRATOR")) {
+            let supmember = message.mentions.members.first()
 
 
 
-    let announcementchannel = message.guild.channels.cache.find(c => c.name === "team-chat")
-    if (!announcementchannel) return;
+            let announcementchannel = message.guild.channels.cache.find(c => c.name === "team-chat")
+            if (!announcementchannel) return;
 
-    announcementchannel.send(`${supmember}, Congratulations on passing your application for the Support Team.  We expect you to be as active as the pilots and working hard.\n\nIF YOU APPLIED FOR MEDIA: Every photo you take is to be submitted in #media-submissions for inspection. Keep in mind that photos or videos MUST be 720p or higher quality; it cannot show ANY GUIs, mouse icons or usernames.`);
-
+            announcementchannel.send(`${supmember}, Congratulations on passing your application for the Support Team.  We expect you to be as active as the pilots and working hard.\n\nIF YOU APPLIED FOR MEDIA: Every photo you take is to be submitted in #media-submissions for inspection. Keep in mind that photos or videos MUST be 720p or higher quality; it cannot show ANY GUIs, mouse icons or usernames.`);
+        } else message.reply(", you do not have permission to use this command.")
     } else return;
 }
