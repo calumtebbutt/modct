14ab252fbd5fd1d67a6310838b846a90140052d0
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-07-21 18:20:04 +0100

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,55 +1,69 @@
 const { Client, Collection, MessageEmbed } = require('discord.js');
 
 module.exports.config = {
-    name: "mute",
-    aliases: []
+  name: "mute",
+  aliases: []
 }
 
 
 module.exports.run = async (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_ROLES")) {
-        return message.reply(
-          " you do not have permission to mute anyone"
-        );
-      }
-  
-      if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-        return message.reply("I do not have permission to manage roles.");
-      }
+  if (!message.member.hasPermission("MANAGE_ROLES")) {
+    return message.reply(
+      " you do not have permission to mute anyone"
+    );
+  }
 
-      const user = message.mentions.members.first();
+  if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+    return message.reply("I do not have permission to manage roles.");
+  }
 
-    if (!user) {
-      return message.reply(
-        " please specify a user to mute."
-      );
-    }
- 
-    if(user.id === message.author.id) {
-      return message.channel.send("I am unable to mute a bot.");
-    }
+  const muteuser = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-    let reason = args.slice(1).join(" ")
-    
-    
-    if(!reason) {
-      return message.reply(" please give the reason to mute the member.")
-    }
+  if (!muteuser) {
+    return message.reply(
+      " please specify a user to mute."
+    );
+  }
 
-    let muterole = message.guild.roles.cache.find(x => x.name === "Muted")
-    
-    
-      if(!muterole) {
-      return message.reply(" this server does not have a role with name `Muted`. Please create this role and set permissions.")
-    }
+  if (muteuser.id === message.author.id) {
+    return message.channel.send("I am unable to mute mentioned user.");
+  }
 
-    if(user.roles.cache.has(muterole)) {
-        return message.reply(" user is already muted.")
-      }
+  let reason = args.slice(1).join(" ")
 
-      user.roles.add(muterole)
-    
-await message.reply(`You muted **${message.mentions.users.first().username}** For \`${reason}\``)
-    
-    user.send(`You are muted in **${message.guild.name}** for reason: \`${reason}\``)
+
+  if (!reason) {
+    return message.reply(" please give the reason to mute the member.")
+  }
+
+  let muterole = message.guild.roles.cache.find(x => x.name === "Muted")
+
+
+  if (!muterole) {
+    return message.reply(" this server does not have a role with name `Muted`. Please create this role and set permissions to each channel.")
+  }
+
+  if (muteuser.roles.cache.has(muterole)) {
+    return message.reply(" user is already muted.")
+  }
+
+  muteuser.roles.add(muterole)
+
+  const muteembed = new MessageEmbed()
+  .setTitle("**User Muted**")
+  .setColor("#ff9900")
+  .addField("User:", `${message.mentions.members.first() || message.guild.members.cache.get(args[0])}`)
+  .addField("Muted by:", `${message.author.tag}`)
+  .addField("Reason:", `${reason}`)
+  .setTimestamp()
+  .setFooter("CTModeration");
+
+
+  await message.reply(`You muted **${message.mentions.users.first().username}** For \`${reason}\``)
+
+
+  let mutelog = message.guild.channels.cache.find(c => c.name === "chat-logs")
+  if (!mutelog) return;
+
+  mutelog.send(muteembed);
 }
