179b0c601b495a8f3dc613fd93c7b73a730f6306
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-07-21 18:20:25 +0100

    Update unmute.js

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -19,7 +19,7 @@ module.exports.run = async (client, message, args) => {
     }
 
 
-    const user = message.mentions.members.first();
+    const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
     if (!user) {
         return message.reply(
@@ -40,4 +40,19 @@ module.exports.run = async (client, message, args) => {
     await message.reply(`**${message.mentions.users.first().username}** has been unmuted`)
 
     user.send(`You are now unmuted from **${message.guild.name}**`)
+
+
+  const unmuteembed = new MessageEmbed()
+  .setTitle("**User Unmuted**")
+  .setColor("#ff9900")
+  .addField("User:", `${message.mentions.members.first() || message.guild.members.cache.get(args[0])}`)
+  .addField("Unmuted by:", `${message.author.tag}`)
+  .setTimestamp()
+  .setFooter("CTModeration");
+
+
+  let unmutelog = message.guild.channels.cache.find(c => c.name === "chat-logs")
+  if (!unmutelog) return;
+
+  unmutelog.send(unmuteembed);
 }
