603d3aed2c042c762ca7aaf9936bc98959a6e627
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-11-18 21:52:17 +0000

    Update ratpfaq.js

--- commands/groups/ratp/ratpfaq.js
+++ commands/groups/ratp/ratpfaq.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
 
     const ratpfaqEmbed = new MessageEmbed()
     .setTitle("RATP Dev London - Frequently Asked Questions")
-    .setDescription("**Can I be a HR?**\nIf we're hiring for HRs/MRs, the position will be advertised in the #applications channel.\n\n**When is the next meeting?**\nMeetings are advertised on our Group Shout and Discord server.")
+    .setDescription("**Can I be a HR?**\nIf we're hiring for HRs/MRs, the position will be advertised in the #applications channel.\n\n**When is the next meeting?**\nMeetings are advertised on our Group Shout and Discord server.\n\n**Is the game mobile compatible?**\nNo and it never will be.")
     .setColor("#24bfab")
     .setFooter("CTModeration - RATP Dev London");
 
