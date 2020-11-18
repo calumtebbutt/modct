81725b8d4a1b71d189bd56657e42d8d042a75f9d
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-11-18 21:06:30 +0000

    Update ratpfleet.js

--- commands/groups/ratp/ratpfleet.js
+++ commands/groups/ratp/ratpfleet.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
 
     const ratpfleetEmbed = new MessageEmbed()
     .setTitle("RATP Dev London - Fleet")
-    .setDescription("To be announced soon...")
+    .setDescription("- Marhsall Dart 9.9m\n- E200 9.6m & 10.8m\n- E200 MMC 10.2m\n- E400 MMC 10.5m\n- LT 11.2m\n\nOther buses are coming soon and once the game is opened you will see them tested around the map by the developers.")
     .setColor("#24bfab")
     .setFooter("CTModeration - RATP Dev London");
 
