03310b8802b22335b9133baf31b643a51cfd1d4c
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-11-18 21:07:52 +0000

    Update ratpoperators.js

--- commands/groups/ratp/ratpoperators.js
+++ commands/groups/ratp/ratpoperators.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
         
     const ratpoperatorsEmbed = new MessageEmbed()
     .setTitle("RATP Dev London - L&E V3 Operators")
-    .setDescription("**RATP Dev London**\nDiscord: https://discord.gg/4CaCKa2\nGroup Link: https://www.roblox.com/groups/5841116/RATP-Dev-London#!/about\n\n**Stagecoach Group UK**\nDiscord: https://discord.gg/a4Tj8dC\nGroup Link: https://www.roblox.com/groups/4470266/SC-Stagecoach-Group-UK#!/about\n\n**Sullivan Buses**\nDiscord: https://discord.gg/pFwxJHm\nGroup Link: https://www.roblox.com/groups/4033033/Sullivanbuses#!/about\n\n**Selkent**\nDiscord: https://discord.gg/nY8yCT2\nGroup Link: https://www.roblox.com/groups/4772685/SC-Stagecoach#!/about\n\n**SunRise Coffee**\nDiscord: https://discord.gg/PMRz96u\nGroup Link: https://www.roblox.com/groups/1058031/SunRise-Coffee#!/about")
+    .setDescription("**RATP Dev London**\nDiscord: https://discord.gg/4CaCKa2\nGroup Link: https://www.roblox.com/groups/5841116/RATP-Dev-London#!/about\n\n**Stagecoach Group UK**\nDiscord: https://discord.gg/a4Tj8dC\nGroup Link: https://www.roblox.com/groups/4470266/SC-Stagecoach-Group-UK#!/about\n\n**Sullivan Buses**\nDiscord: https://discord.gg/pFwxJHm\nGroup Link: https://www.roblox.com/groups/4033033/Sullivanbuses#!/about\n\n**Selkent**\nDiscord: https://discord.gg/nY8yCT2\nGroup Link: https://www.roblox.com/groups/4772685/SC-Stagecoach#!/about\n\n**SunRise Coffee (Coming soon...)**\nDiscord: https://discord.gg/PMRz96u\nGroup Link: https://www.roblox.com/groups/1058031/SunRise-Coffee#!/about")
     .setColor("#24bfab")
 
     .setFooter("CTModeration - RATP Dev London");
