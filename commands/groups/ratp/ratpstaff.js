741a475c3bcd3941dcb4101ff83546500b479ca0
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-11-18 21:30:30 +0000

    Update ratpstaff.js

--- commands/groups/ratp/ratpstaff.js
+++ commands/groups/ratp/ratpstaff.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
 
     const ratpstaffEmbed = new MessageEmbed()
     .setTitle("RATP Dev London - Staff")
-    .setDescription("**HRs**\nreecerod\nBrickBusEllis\nAlexDev_exe\nH1_Z1z\namazingguy377\nDD_Sim\nRebelkopGamer206\nPhotoBlockTrollz\nFletchyboy100\nmax8gaming\nBritishAviator_RBX\nTowerGateway\nthemister18\nKacperEpic\n\n**Engineering Team**\nx3fklazomaniac\nMike_7668\nsenyo44\nBrian_Sedano\nbossmaneternity\nkyrovia\nTsunamiD\nthegreeknotebook\nMrTomasBoy\nrailwork\nJPYukiRBX\n\n**Execs**\nCalumT01 *- Deputy Chairman*\niiReece98 *- Chairman*")
+    .setDescription("**HRs**\nreecerod\nBrickBusEllis\nAlexDev_exe\nH1_Z1z\namazingguy377\nDD_Sim\nRebelkopGamer206\nPhotoBlockTrollz\nFletchyboy100\nmax8gaming\nBritishAviator_RBX\nTowerGateway\nthemister18\nKacperEpic\nixMidnight_Rosieix\nczllvm\n\n**Engineering Team**\nx3fklazomaniac\nMike_7668\nsenyo44\nBrian_Sedano\nbossmaneternity\nkyrovia\nTsunamiD\nthegreeknotebook\nMrTomasBoy\nrailwork\nJPYukiRBX\n\n**Execs**\nCalumT01 *- Deputy Chairman*\niiReece98 *- Chairman*")
     .setColor("#24bfab")
     .setFooter("CTModeration - RATP Dev London");
 
