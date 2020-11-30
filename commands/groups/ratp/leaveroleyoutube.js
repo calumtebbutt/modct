c2eb52485cb8a6e7c0677171a02a3dfc1de7d7a6
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-11-30 20:44:55 +0000

    Update leaveroleyoutube.js

--- commands/groups/ratp/leaveroleyoutube.js
+++ commands/groups/ratp/leaveroleyoutube.js
@@ -10,6 +10,8 @@ module.exports.run = async (client, message, args) => {
 
     let youtube = message.guild.roles.cache.find(x => x.name === "YouTube"){
         
+        let user = message.author
+        
         if (user.roles.cache.has(youtube)) {
         user.roles.remove(youtube)
         return message.reply(", you've left the YouTube role.")
