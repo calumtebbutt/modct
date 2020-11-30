6fffe4b9f59670ff5abbcc1c6e3236229e3c4335
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2020-11-30 20:53:18 +0000

    Update and rename leaveroleyoutube.js to ratpyoutube.js

---
+++ commands/groups/ratp/ratpyoutube.js
@@ -0,0 +1,25 @@
+const { Client, Collection, MessageEmbed } = require('discord.js');
+
+module.exports.config = {
+    name: "leaveyoutube",
+    aliases: []
+}
+
+
+module.exports.run = async (client, message, args) => {
+
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+        return message.channel.send("I do not have permission to manage roles.");
+    }
+    
+    let user = message.author();
+    
+    let youtube = message.guild.roles.cache.find(x => x.name === "YouTube");
+
+
+    if (user.roles.cache.has(youtube)) {
+        user.roles.remove(youtube)
+    } else return
+    message.reply(", you do not have the YouTube role.")
+    
+}
