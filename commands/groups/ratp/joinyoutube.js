521fe7c2ea4e1e0d4ace98ca93d317eb119049f4
Calum T <65409206+calumtebbutt@users.noreply.github.com>
2021-01-04 15:27:58 +0000

    Update joinyoutube.js

--- commands/groups/ratp/joinyoutube.js
+++ commands/groups/ratp/joinyoutube.js
@@ -1,1 +1,26 @@
+const { Client, Collection, MessageEmbed } = require('discord.js');
+
+module.exports.config = {
+    name: "joinyoutube",
+    aliases: []
+}
+
+module.exports.run = async (client, message, args) => {
+
+
+    if (message.guild.id === '693509227343249418') {
+
+        let youtube = message.guild.roles.cache.get('783061666081275981')
+
+        const member = message.member
+
+
+        if (!youtube) {
+            member.roles.add(youtube)
+            message.reply(' added role **YouTube**.')
+        }
+
+    }
+
+}
 
